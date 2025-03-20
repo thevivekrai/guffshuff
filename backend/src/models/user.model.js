4a3b441f2eb3b4c9ecd00fc60ebcccf7e826bd73
thevivekrai <159144612+thevivekrai@users.noreply.github.com>
2025-03-21 02:25:24 +0530

    Update user.model.js

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -29,6 +29,17 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: {
+        validator: function (v) {
+          return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(v);
+        },
+        message: (props) => `${props.value} is not a valid email address!`,
+      },
+    },
   },
   { timestamps: true }
 );
